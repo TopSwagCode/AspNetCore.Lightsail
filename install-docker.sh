e6c70067b37a80bd57a6c24520855864940190f0
Joshua Ryder <joshua.ryder@outlook.com>
2020-07-18 19:04:02 +0200

    Install docker

--- install-docker.sh
+++ install-docker.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# install latest version of docker the lazy way
-curl -sSL https://get.docker.com | sh
+# Setup Repository
+sudo apt-get update
+echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+# Install Docker
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # make it so you don't need to sudo to run docker commands
-usermod -aG docker ubuntu
+sudo usermod -aG docker ubuntu
 
 # install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # copy the dockerfile into /srv/docker 
 # if you change this, change the systemd service file to match
