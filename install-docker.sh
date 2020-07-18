c3274102938eb0b122c6948c7f22a27944c0f00c
Joshua Ryder <joshua.ryder@outlook.com>
2020-07-18 19:45:38 +0200

    install easy docker test

--- install-docker.sh
+++ install-docker.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Setup Repository
-sudo apt-get update
-echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo apt-get update
+#echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
+#sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Install Docker
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+#sudo apt-get update
+#sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get update && sudo apt-get install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # make it so you don't need to sudo to run docker commands
 sudo usermod -aG docker ubuntu
