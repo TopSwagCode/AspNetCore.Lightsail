b654e1e2298c87dd1606dc438239fccf2dde4c83
Joshua Ryder <joshua.ryder@outlook.com>
2020-07-23 20:32:21 +0200

    testing 123 final

--- launchscript.sh
+++ launchscript.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -o ./lightsail.sh https://raw.githubusercontent.com/TopSwagCode/lightsail.aspnetcore/master/install-docker.sh | sh
+curl -o ./lightsail.sh https://raw.githubusercontent.com/TopSwagCode/lightsail.aspnetcore/master/setup-app.sh | sh
 chmod +x ./lightsail.sh 
 ./lightsail.sh
