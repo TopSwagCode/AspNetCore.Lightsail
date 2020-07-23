5f6bc79ff693584e4a1fd4029f3ebb042813349f
Joshua Ryder <joshua.ryder@outlook.com>
2020-07-23 20:15:54 +0200

    Final steps

--- install-docker.sh
+++ install-docker.sh
@@ -6,26 +6,24 @@ curl -sSL https://get.docker.com | sh
 # make it so you don't need to sudo to run docker commands
 usermod -aG docker ubuntu
 
-# make it so you don't need to sudo to run docker commands
-usermod -aG docker ubuntu
-
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# copy the dockerfile into /srv/docker 
+# download source code from github and store it in /srv/docker
 mkdir /srv/docker
 curl -o /srv/docker/release.zip -LJO https://github.com/TopSwagCode/lightsail.aspnetcore/archive/master.zip
 cd /srv/docker
-# Unzip and move files into current folder
+
+# install Zip and unzip and move files into current folder
 apt-get update
 apt-get install zip unzip -qy
 unzip release.zip
 mv lightsail.aspnetcore-master/* .
 
-## !!!!! TODO !!!!! Move docker-compose-app.service file to  /etc/systemd/system/ folder!
-#curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose-app.service
-#systemctl enable docker-compose-app
+# move linux service file to /etc/systemd/system/ and enable the service
+mv docker-compose-app.service /etc/systemd/system/
+systemctl enable docker-compose-app
 
-# start up the application via docker-compose. !!!! TODO !!!!! Use build. To ensure we always get latests version!.
-docker-compose -f docker-compose.yml up -d
+# start up the application via docker-compose.
+docker-compose -f docker-compose.yml up -d --build
